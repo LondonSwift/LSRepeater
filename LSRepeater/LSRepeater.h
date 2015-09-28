d35acc6b86b24416bf610abb5cd30a7936516bb7
= <mcbontempi@darens-MacBook-Pro-2.local>
2015-09-28 09:11:29 +0100

    initial commit

---
+++ LSRepeater/LSRepeater.h
@@ -0,0 +1,19 @@
+//
+//  LSRepeater.h
+//  LSRepeater
+//
+//  Created by Daren David Taylor on 28/09/2015.
+//  Copyright © 2015 LondonSwift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LSRepeater.
+FOUNDATION_EXPORT double LSRepeaterVersionNumber;
+
+//! Project version string for LSRepeater.
+FOUNDATION_EXPORT const unsigned char LSRepeaterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSRepeater/PublicHeader.h>
+
+
